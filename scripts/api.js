91ea99bce9b2ae518b6568fff0842c8932e9b792
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-01-25 16:53:44 -0500

    create api routes

--- scripts/api.js
+++ scripts/api.js
@@ -19,3 +19,63 @@ export const logIn = (data) => {
         body: JSON.stringify(data)
     })
 }
+
+//=============
+
+export const createRecipe = (data) => {
+    return fetch('http://localhost:3000/recipes', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+export const indexRecipes = () => {
+    return fetch('http://localhost:3000/recipes')
+}
+
+export const showRecipe = (recipeId) => {
+    return fetch(`http://localhost:3000/recipes/${recipeId}`)
+} 
+
+export const updateRecipe = (data) => {
+    return fetch(`http://localhost:3000/recipes/${recipeId}`, {
+        method: 'PATCH',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+export const deleteRecipe = (recipeId) => {
+    return fetch(`http://localhost:3000/recipes/${recipeId}`, {
+        method: 'DELETE'
+    })
+}
+
+export const addIngredient = (data, recipeId) => {
+    return fetch(`http://localhost:3000/ingredients/${recipeId}`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+export const deleteIngredient = (data, recipeId) => {
+    return fetch(`http://localhost:3000/ingredients/${recipeId}`, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ingredientId)
+    })
+}
